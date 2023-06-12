8b134cd736787457a75c1c599983e627fab31da8
PolyakVladislav <9725860@gmail.com>
2023-06-12 17:28:43 +0300

    Update to Project

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 const express = require('express');
-const PORT= 70;
-const ejs= require('ejs');
-const path= require('path');
+const PORT = 70;
+const ejs = require('ejs');
+const path = require('path');
 const app = express();
-const mongoose = require('mongoose');
-app.use(express.static(path.join(__dirname,"public")));
-app.use(express.static(path.join(__dirname,"models")));
 
-app.set("view engine","ejs");
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/",(req,res) =>{
-  res.render("loginpage");
+const {default:mongoose} = require("mongoose");
+mongoose.connect('mongodb+srv://dormatana101:Dormatana054@sportify.m6md4z5.mongodb.net/', {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
 });
 
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
 
+const loginRouter = require('./routes/login');
+app.use('/', loginRouter);
 app.get("/Register",(req,res) =>{
   res.render("Register");
 });
-app.listen(PORT,()=>console.log("Server started on port 70"));
+app.listen(PORT, () => console.log('Server started on port 70'));
