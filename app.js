23fb9435dcc6622b4a82202963ff0d7320de0a42
Ofek Segman <ofeksegman@gmail.com>
2023-06-12 16:23:38 +0300

    Update register

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ const PORT= 70;
 const ejs= require('ejs');
 const path= require('path');
 const app = express();
-
+const mongoose = require('mongoose');
 app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname,"models")));
 
 app.set("view engine","ejs");
 
 app.get("/",(req,res) =>{
   res.render("loginpage");
+});
+
+
+app.get("/Register",(req,res) =>{
+  res.render("Register");
 });
 app.listen(PORT,()=>console.log("Server started on port 70"));
