aa6ebc44700bed983bec4aa7bb707ec34c71dd63
Nati Ezra <natanelezra@gmail.com>
2023-06-12 22:15:02 +0300

    .

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, 'models')));
 const {default:mongoose} = require("mongoose");
 mongoose.connect('mongodb+srv://dormatana101:Dormatana054@sportify.m6md4z5.mongodb.net/', {
   useUnifiedTopology: true,
