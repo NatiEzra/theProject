c787597c6c7d0d143fb3ea1d491754efcb3ae55a
Ofek Segman <ofeksegman@gmail.com>
2023-06-12 16:44:46 +0300

    update

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require('express');
-const PORT= 70;
-const ejs= require('ejs');
-const path= require('path');
+const PORT = 70;
+const ejs = require('ejs');
+const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.set("view engine","ejs");
-
-app.get("/",(req,res) =>{
-  res.render("loginpage");
+const {default:mongoose} = require("mongoose");
+mongoose.connect('mongodb+srv://dormatana101:Dormatana054@sportify.m6md4z5.mongodb.net/', {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
 });
-app.listen(PORT,()=>console.log("Server started on port 70"));
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+
+const loginRouter = require('./routes/login');
+app.use('/', loginRouter);
+app.get("/Register",(req,res) =>{
+  res.render("Register");
+});
+app.listen(PORT, () => console.log('Server started on port 70'));
