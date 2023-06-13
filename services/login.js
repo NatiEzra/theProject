dff55278ee3d08db3b74799f2df797f7046370a1
Ofek Segman <ofeksegman@gmail.com>
2023-06-13 18:29:14 +0300

    update-

--- services/login.js
+++ services/login.js
@@ -1,15 +1,42 @@
 const User = require("../models/User");
 
 async function login(username, password) {
-    const user = await User.findOne({ _id: username, password });
-    return user != null
+    console.log(username);
+    console.log(password);
+    const user = await User.findOne({username:username,password:password });
+    if(user!==null)
+        return user
+    else
+    {
+        console.log("here2");
+        const user2 = await User.findOne({ username: username});
+        if(user2==null)
+        {
+              /*console.log("here3");
+                document.getElementById('userError').classList.add('Error_start');
+                console.log("here4");
+                document.getElementById('userError').classList.remove('Error_NoShow');
+                setTimeout(() => {
+                    document.getElementById('userError').classList.remove('Error_start');
+                    document.getElementById('userError').classList.add('Error_NoShow');
+                }, "1300");
+                return;
+                */
+                return "Invalid username";
+    }
+     else {
+      return "Invalid password";
+    } 
+      
+           
+    }
 }
 
 async function register(username, password) {
 
     const user = new User({
         username: username,
-        password
+        password:paswword
     });
 
     await user.save()        
