9159f68dc32e935148accef6971e63d362af23f4
PolyakVladislav <9725860@gmail.com>
2023-06-18 17:07:18 +0300

    Eddit for login page. Empty check

--- public/js/login.js
+++ public/js/login.js
@@ -28,4 +28,52 @@ function redirectTo(url) {
       window.open(url, '_blank');
     }
   }
+
+{// A function that checks if the username and password are empty.
+  document.addEventListener('DOMContentLoaded', function () {
+    var loginForm = document.getElementById('login-form');
+    var usernameInput = document.getElementById('username');
+    var passwordInput = document.getElementById('password');
   
+    loginForm.addEventListener('submit', function (event) {
+      var isUsernameEmpty = usernameInput.value.trim() === '';
+      var isPasswordEmpty = passwordInput.value.trim() === '';
+  
+      if (isUsernameEmpty || isPasswordEmpty) {
+        event.preventDefault();
+  
+        if (isUsernameEmpty) {
+          usernameInput.style.borderColor = 'red';
+          usernameInput.placeholder = 'You did not enter a username.';
+        } else {
+          resetInputStyle(usernameInput);
+        }
+  
+        if (isPasswordEmpty) {
+          passwordInput.style.borderColor = 'red';
+          passwordInput.placeholder = 'You did not enter a password.';
+        } else {
+          resetInputStyle(passwordInput);
+        }
+      }
+    });
+  
+    usernameInput.addEventListener('input', function () {
+      resetInputStyle(usernameInput);
+    });
+  
+    passwordInput.addEventListener('input', function () {
+      resetInputStyle(passwordInput);
+    });
+  
+    function resetInputStyle(input) {
+      input.style.borderColor = '';
+      if (input === usernameInput) {
+        input.placeholder = 'Type your Username';
+      } else if (input === passwordInput) {
+        input.placeholder = 'Type your password';
+      }
+    }
+  });
+  
+}
