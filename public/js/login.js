dbddd069707e0b1ac147086d5066579227ff0252
PolyakVladislav <9725860@gmail.com>
2023-06-18 15:04:35 +0300

    js login function

--- public/js/login.js
+++ public/js/login.js
@@ -16,3 +16,16 @@ async function initMap() {
 }
 
 initMap();
+function redirectTo(url) {
+    if (url !== 'http://localhost:70/login') {
+      event.preventDefault();
+      return;
+    }
+    window.location.href = url;
+  }
+  function openSocialLogin(url) {
+    if (url === 'https://www.facebook.com/' || url === 'https://www.google.com') {
+      window.open(url, '_blank');
+    }
+  }
+  
