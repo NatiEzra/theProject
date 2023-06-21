a8b457135c359cb2ac309e73930dab09b8391d77
Revital09 <128730030+Revital09@users.noreply.github.com>
2023-06-21 19:18:38 +0300

    change

--- controllers/mainpage.js
+++ controllers/mainpage.js
@@ -7,89 +7,108 @@ function Mainpage(req, res) {
     } else {
       const header = `
       <div class="header">
-        <div class="logo">
-          <a href="./main page.html">  <!--attribute to the main page URL -->
-            <img src="./img/logo_transparent2.png" alt="Sportify Logo">
+      <div class="logo">
+        <a href="./Mainpage.html">
+          <img
+            src="../Images/logo_transparent2.png"
+            alt="Sportify Logo"
+          />
+        </a>
+      </div>
+      <div class="search-bar">
+        <input type="text" placeholder="Search items" />
+        <button class="register-button">
+          <a href="#register">
+            <img src="../Images/user2.png" alt="Register Icon" />
+            <div class="menu">
+              <ul>
+                <li>
+                  <a class="login-join" ;href="http://localhost:70/login">Login</a><span>+ </span
+                  ><a class="login-join" ;href="">Join</a>
+                </li>
+                <li>
+                
+                   
+        <button class="like-button">
+          <a href="#liked-items">
+            <img src="../Images/heart.png" alt="Register Icon" />
           </a>
-        </div>
-        <div class="search-bar">
-          <input type="text" placeholder="Search items">
-          <button class="register-button">
-            <a href="#register">
-              <img src="./img/user2.png" alt="Register Icon">
-            </a>
-          </button>
-          <button class="like-button">
-            <a href="#liked-items">
-              <img src="./img/heart.png" alt="Register Icon">
-            </a>
-          </button>
-          <button class="bag-button">
-            <a href="#bag">
-              <img src="./img/bag.png" alt="Register Icon">
-            </a>
-          </button>
-        </div>
-      </div>`;
+        </button>
+        <button class="bag-button">
+          <a href="#bag">
+            <img src="../Images/bag.png" alt="Register Icon" />
+          </a>
+        </button>
+      </div>
+    </div>`;
       
       const dropdown = `
-      <div class="dropdown">
+      <div class="navigation">
+    <div class="dropdown">
+      <a class="active" href="#New">New & Features</a>
+      <div class="dropdown-content">
         <a href="#Men">Men</a>
-        <div class="dropdown-content">
-          <div class="sub-dropdown">
-            <a href="#shirts">Shirts</a>
-            <ul>
-              <li><a href="#tank-tops">Tank Tops</a></li>
-              <li><a href="#t-shirts">T-Shirts</a></li>
-            </ul>
-          </div>
-          <div class="sub-dropdown">
-            <a href="#pants">Pants</a>
-            <ul>
-              <li><a href="#tank-tops">Jeans</a></li>
-              <li><a href="#t-shirts">Shorts</a></li>
-            </ul>
-          </div>
-          <div class="sub-dropdown">
-            <a href="#shoes">Shoes</a>
-            <ul>
-              <li><a href="#tank-tops">Sneakers</a></li>
-              <li><a href="#t-shirts">Slippers</a></li>
-            </ul>
-          </div>
-        </div>
-      </div>`;
+        <a href="#Women">Women</a>
+      </div>
+    </div>
+    
+    <div class="dropdown">
+      <a href="#Men">Men</a>
+      <div class="dropdown-content">
+      <a href="#shirts">Shirts</a>
+      <a href="#pants">Pants</a>
+      <a href="#shoes">Shoes</a>
+      </div>
+    </div>
+
+    <div class="dropdown">
+      <a href="#Women">Women</a>
+      <div class="dropdown-content">
+      <a href="#shirts">Shirts</a>
+      <a href="#pants">Pants</a>
+      <a href="#shoes">Shoes</a>
+      </div>
+    </div>
+
+  <div class="dropdown">
+    <a href="#Sale">Sale</a>
+    <div class="dropdown-content">
+      <a href="#Men">Men</a>
+      <a href="#Women">Women</a>
+      </div>
+  </div>
+  </div>` ;
       
       const footer = `
       <div class="footer">
         <h3>Follow us</h3>
         <button class="instagram-button">
           <a href="#instagram">
-            <img src="./img/instagram.png" alt="instagram Icon">
+            <img src="./images/instagram.png" alt="instagram Icon">
           </a>
         </button>
         <button class="facebook-button">
           <a href="#facebook">
-            <img src="./img/facebook.png" alt="facebook Icon">
+            <img src="./images/facebook.png" alt="facebook Icon">
           </a>
         </button>
         <h3>About</h3>
         <button class="location-button">
           <a href="#location">
-            <img src="./img/pin.png" alt="location Icon">
+            <img src="./images/pin.png" alt="location Icon">
           </a>
         </button>
         <h3>Management</h3>
         <button class="management-button">
           <a href="#management">
-            <img src="./img/secure.png" alt="management Icon">
+            <img src="./images/secure.png" alt="management Icon">
           </a>
         </button>
       </div>`;
       
       const modifiedHTML = data
         .replace('<div class="header">', header)
-        .replace('<div class="dropdown">', dropdown)
+        .replace('<div class="navigation">', dropdown)
         .replace('<div class="footer">', footer);
     console.log(modifiedHTML);
       res.send(modifiedHTML);
