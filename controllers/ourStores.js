2163c2a909a093288cee8332948ecc2c3818c271
Dor <dormatana101@gmail.com>
2023-06-22 17:34:32 +0300

    add cconfig

--- controllers/ourStores.js
+++ controllers/ourStores.js
@@ -2,7 +2,8 @@ const adress = require("../models/StoreLocation");
 const storeService = require("../services/ourStores");
 require('custom-env').env(process.env.NODE_ENV, './config');
 function Storepgage(req, res) {
-     res.render('ourStores',{apikey:process.env.apikey});
+      const apikey=process.env.apikey;
+     res.render('ourStores',{apikey:apikey});
    }
 async function stores(req, res,next) {
    const storeLoc=await storeService.getmap();
