d116c95e0bb79ed0d1cfd906bed83114ca6982ce
Revital09 <128730030+Revital09@users.noreply.github.com>
2023-06-22 15:16:47 +0300

    rev

--- controllers/mainpage.js
+++ controllers/mainpage.js
@@ -113,9 +113,10 @@ function Mainpage(req, res) {
 }
 function Cartpage(req, res) { res.render("ShoppingCart", {}) }
 
-function Salepage(req,res){
-  res.render("Salepage", {})
-}
+function Salepage(req,res){ res.render("Salepage", {})}
+
+function renderStores(req,res){ res.render("ourStores", {})}
+
 module.exports = {
 Mainpage,
 Cartpage,
