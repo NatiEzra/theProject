909ce912e8da519d3ccdddef2bb173f3f1bac22e
Dor <dormatana101@gmail.com>
2023-06-24 17:30:44 +0300

    fix logout

--- controllers/ourStores.js
+++ controllers/ourStores.js
@@ -1,9 +1,13 @@
 const adress = require("../models/StoreLocation");
 const storeService = require("../services/ourStores");
+const logedin = require("./login");
 require('custom-env').env(process.env.NODE_ENV, './config');
 function Storepgage(req, res) {
       const apikey=process.env.apikey;
-     res.render('ourStores',{apikey:apikey});
+      let username = logedin.isLoggedIn(req, res);
+      if (username == null) loggedIn = false;
+      else loggedIn = true;
+     res.render('ourStores',{apikey:apikey , loggedIn: loggedIn,username: username});
    }
 async function stores(req, res,next) {
    const storeLoc=await storeService.getmap();
